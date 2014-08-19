05e4cba42154b6438ae9105d7bd925ef7cafa5cb
Haydon Murdoch <hgt.murdoch@gmail.com>
2014-08-19 15:23:56 +0100

    Directory restructure

--- ch01/ch01_ex02.py
+++
@@ -1,7 +0,0 @@
-# Write and save a program that prints out your
-# name and waits for the user to press the Enter
-# key before the program ends. Then, run the
-# program by double-clicking its icon.
-
-print("Haydon")
-input("Please press enter to exit the program")
