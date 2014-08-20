a1699c6c3812fb4a4e2a5526764d4aaee56b468f
Haydon Murdoch <hgt.murdoch@gmail.com>
2014-08-20 10:03:55 +0100

    Slightly modify comments

--- ch04/random_access.py
+++ ch04/random_access.py
@@ -6,8 +6,8 @@ import random # importing random module
 word = "index" # setting word value
 print("The word is, ", word, "\n")
 
-high = len(word) # max index allowed
-low = -len(word) # min index allowed
+high = len(word) # max index allowed  (5)
+low = -len(word) # min index allowed (-5)
 
 for i in range(10): # do this ten times
     position = random.randrange(low, high) # get a random string index
