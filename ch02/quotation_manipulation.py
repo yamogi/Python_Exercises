fc0c9a1b832e4ab02d0cb803d514009b8a1ea26c
Haydon Murdoch <hgt.murdoch@gmail.com>
2014-08-23 00:25:50 +0100

    Add comments

--- ch02/quotation_manipulation.py
+++ ch02/quotation_manipulation.py
@@ -5,21 +5,21 @@
 quote = "I think there is a world market for maybe five computers."
 
 print("Original quote:")
-print(quote)
+print(quote) # displays str value
 
 print("\nIn uppercase:")
-print(quote.upper())
+print(quote.upper()) # converts to upper
 
 print("\nIn lowercase:")
-print(quote.lower())
+print(quote.lower()) # converts to lower
 
 print("\nAs a title:")
-print(quote.title())
+print(quote.title()) # capitalises first letter of each word
 
 print("\nWith a minor replacement:")
-print(quote.replace("five", "millions of"))
+print(quote.replace("five", "millions of")) # replaces first with second
 
 print("\nOriginal quote is still:")
-print(quote)
+print(quote) # displays str value
 
 input("\n\nPress the enter key to exit.")
