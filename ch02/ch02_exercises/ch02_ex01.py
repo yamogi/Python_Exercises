e6041337d0ceadf5765babcb96a6cdde765ce735
Haydon Murdoch <hgt.murdoch@gmail.com>
2014-08-23 00:28:26 +0100

    Add initial comments, change formatting

--- ch02/ch02_exercises/ch02_ex01.py
+++ ch02/ch02_exercises/ch02_ex01.py
@@ -1,25 +1,26 @@
-# Create a list of legal and illegal variable
-# names. Describe why each is either legal or
-# illegal. Next, create a list of "good" and
-# "bad" legal variable names. Describe why each
-# is either a good or bad choice for a variable
-# name.
+# ch02_ex01.py
+#
+# Create a list of legal and illegal variable names. Describe why each is
+# either legal or illegal. Next, create a list of "good" and "bad" legal
+# variable names. Describe why each is either a good or bad choice for a
+# variable name.
+#
 
-# Legal:
-# - var
-# - var1
-# - var_1
+#   Legal:
+#       - var
+#       - var1
+#       - var_1
 #
-# Illegal:
-# - var!
-# - 1var
+#   Illegal:
+#       - var!
+#       - 1var
 #
-# Legal - good:
-# - first_name
-# - last_name
-# - username
-# - password
+#   Legal (good):
+#       - first_name
+#       - last_name
+#       - username
+#       - password
 #
-# Legal - bad
-# - x
-# - first_name-of-user_on-the_system
+#   Legal (bad):
+#       - x
+#       - first_name-of-user_on-the_system
