d7ce522cae14ab96deb458dbb1b3ed70de98ccae
Haydon Murdoch <hgt.murdoch@gmail.com>
2014-08-25 09:04:40 +0100

    Fix attributes falling below zero

--- ch05/ch05_exercises/ch05_ex02.py
+++ ch05/ch05_exercises/ch05_ex02.py
@@ -7,14 +7,12 @@
 # attribute and put them back in the pool.
 #
 
-# I NEED TO FIX THIS SO THAT ATTRIBUTES CANNOT FALL BELOW ZERO
-
 MAX_POINTS = 30 # max number of points allowed to be distributed
 points_left = 30 # points remaining
-attribute_names = ("strength",
-                   "health",
-                   "wisdom",
-                   "dexterity") # empty attributes tuple
+#attribute_names = ("strength",
+#                   "health",
+#                   "wisdom",
+#                   "dexterity") # empty attributes tuple (not required)
 attribute_values = [0,0,0,0] # empty attributes list
 loop_max = 4 # setting i values
 
@@ -42,45 +40,66 @@ while choice != 0:
         print("Goodbye.")
     elif choice == 1:
         if points_left > 0:
-            print("Add a point to which attribute?")
-            print("1) str")
-            print("2) hp")
-            print("3) wis")
-            print("4) dex")
+            print("  Add a point to which attribute?")
+            print("  1) str")
+            print("  2) hp")
+            print("  3) wis")
+            print("  4) dex")
         
             add_choice = int(input())
 
             attribute_values[add_choice - 1] += 1
             points_left -= 1
 
+            print()
+
             print("You have", points_left, "points remaining.")
 
             for i in range(loop_max):
                 print("Attribute", str(i+1),
                 "is currently:", attribute_values[i])
+
+            print()
+
         else:
+            print()
             print("No points remaining to distribute!")
+            print()
 
     elif choice == 2:
         if points_left < MAX_POINTS:
-            print("Remove a point from which attribute?")
-            print("1) str")
-            print("2) hp")
-            print("3) wis")
-            print("4) dex")
+            print("  Remove a point from which attribute?")
+            print("  1) str")
+            print("  2) hp")
+            print("  3) wis")
+            print("  4) dex")
 
             rem_choice = int(input())
 
-            attribute_values[rem_choice - 1] -= 1
-            points_left += 1
+            if attribute_values[rem_choice -1] > 0:
+                attribute_values[rem_choice - 1] -= 1
+                points_left += 1
 
-            print("You have", points_left, "points remaining.")
+                print()
 
-            for i in range(loop_max):
-                print("Attribute", str(i+1),
-                "is currently:", attribute_values[i])
+                print("You have", points_left, "points remaining.")
+
+                for i in range(loop_max):
+                    print("Attribute", str(i+1),
+                    "is currently:", attribute_values[i])
+
+                print()
+
+            else:
+                print()
+                print("Attribute cannot fall below zero!")
+                print()
+
         else:
+            print()
             print("Hit max points!!")
+            print()
 
     else:
         print("Wat.")
+        print()
